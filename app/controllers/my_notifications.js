40f2d947cb720ce5caa4c8913de7d1732bbb3873
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-03-15 12:16:33 +0800

    Fix notiifcations tests to cater for changes

--- app/controllers/my_notifications.js
+++ app/controllers/my_notifications.js
@@ -5,6 +5,7 @@ export default offers.extend({
   sortProperties: ["createdAt:desc"],
   sortedModel: Ember.computed.sort("model", "sortProperties"),
   messagesUtil: Ember.inject.service("messages"),
+  store: Ember.inject.service(),
 
   allMessages: Ember.computed(function() {
     return this.store.peekAll("message");
@@ -50,7 +51,7 @@ export default offers.extend({
   allNotifications: Ember.computed("model.@each.state", function() {
     var keys = {};
     var res = [];
-    this.get("sortedModel").forEach(function(message) {
+    this.get("sortedModel").forEach(message => {
       var isPrivate = message.get("isPrivate");
       var key =
         isPrivate + message.get("offer.id") + (message.get("itemId") || "");
@@ -70,6 +71,12 @@ export default offers.extend({
         );
         notification.set("text", message.get("body"));
         notification.set("isSingleMessage", message.get("state") === "unread");
+        if (notification.get("itemId")) {
+          notification.set(
+            "item",
+            this.get("store").peekRecord("item", notification.get("itemId"))
+          );
+        }
 
         keys[key] = notification;
         res.push(notification);
@@ -99,7 +106,7 @@ export default offers.extend({
       }
     },
 
-    async toggleShowUnread() {
+    toggleShowUnread() {
       let showUnread = !this.get("showUnread");
 
       if (!showUnread && !this.get("hasLoadedReadMessages")) {
@@ -107,9 +114,13 @@ export default offers.extend({
         let loadingView = Ember.getOwner(this)
           .lookup("component:loading")
           .append();
-        await this.get("messagesUtil").fetchReadMessages();
-        loadingView.destroy();
-        this.set("hasLoadedReadMessages", true);
+        return this.get("messagesUtil")
+          .fetchReadMessages()
+          .then(() => {
+            this.set("hasLoadedReadMessages", true);
+            this.set("showUnread", showUnread);
+          })
+          .finally(() => loadingView.destroy());
       }
 
       this.set("showUnread", showUnread);
