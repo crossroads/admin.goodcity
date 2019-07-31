d68f98986a6b90fea8432360c2e4b413fc22e740
namrataukirde <namrata.ukirde@kiprosh.com>
2019-07-31 20:48:27 +0530

    add company related changes

---
+++ app/controllers/offer/search_users.js
@@ -0,0 +1,2 @@
+import Ember from "ember";
+import { translationMacro as t } from "ember-i18n";
---
+++ app/models/company.js
@@ -0,0 +1,10 @@
+import DS from "ember-data";
+import Ember from "ember";
+
+var attr = DS.attr;
+
+export default DS.Model.extend({
+  name: attr("string"),
+  crmId: attr("number"),
+  createdById: attr("number")
+});
