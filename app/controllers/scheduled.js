f918149385f2bf7d085e17a77bc7221c691f02b3
AbulAsar <abul.a@kiprosh.com>
2019-08-01 16:45:29 +0530

    remove in_progress and scehduled controller

--- app/controllers/scheduled.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import scheduledOffersMixin from './../mixins/scheduled_offers';
-
-export default Ember.Controller.extend(scheduledOffersMixin, {
-  collectionCount: Ember.computed.alias("collection.length"),
-  ggvCount:        Ember.computed.alias("ggv.length"),
-  dropOffCount:    Ember.computed.alias("dropOff.length")
-});
