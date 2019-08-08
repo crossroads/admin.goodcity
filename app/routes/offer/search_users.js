a2ce9a3110eb743f75335fd464ea1bb0451f4943
namrataukirde <namrata.ukirde@kiprosh.com>
2019-08-08 16:57:29 +0530

    domor ui

--- app/routes/offer/search_users.js
+++ app/routes/offer/search_users.js
@@ -1,3 +1,8 @@
 import AuthorizeRoute from "./../authorize";
 
-export default AuthorizeRoute.extend({});
+export default AuthorizeRoute.extend({
+  model(params) {
+    var offerId = this.modelFor("review_offer").get("id") || params.offer_id;
+    return this.store.peekRecord("offer", offerId);
+  }
+});
