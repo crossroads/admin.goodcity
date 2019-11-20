6e1b6e599cd536cf4de275ceb562e86df52fe84a
akshay gaikwad <akshay@kiprosh.com>
2019-11-20 16:47:53 +0530

    GCW-2642 text cases checking current url

--- tests/acceptance/review-item-test.js
+++ tests/acceptance/review-item-test.js
@@ -147,11 +147,7 @@ test("Assign description same as the assigned Package Name for the first time", 
   andThen(function() {
     assert.equal(
       currentURL(),
-      "/offers/" +
-        offer.id +
-        "/review_item/" +
-        item.id +
-        "/accept?packageTypeUpdated=true"
+      "/offers/" + offer.id + "/review_item/" + item.id + "/accept"
     );
     assert.equal($(".search_label_input input").val(), packageType.get("name"));
     assert.equal($("#comment0").val(), packageType.get("name"));
