7774e8e9f7699318e0f3ac62348d4ef9f34451cf
akshay gaikwad <akshay@kiprosh.com>
2019-12-16 23:23:35 +0530

    GCW-2853 removed unewanted code

--- app/controllers/receive_package.js
+++ app/controllers/receive_package.js
@@ -296,15 +296,6 @@ export default Ember.Controller.extend(AsyncTasksMixin, {
       });
   },
 
-  updateUserDefaultPrinter(printerId) {
-    new AjaxPromise(
-      `/users/${this.get("session.currentUser.id")}`,
-      "PUT",
-      this.get("session.authToken"),
-      { user: { printer_id: printerId } }
-    ).then(data => this.get("store").pushPayload(data));
-  },
-
   // ----- Actions -----
   actions: {
     clearDescription() {
