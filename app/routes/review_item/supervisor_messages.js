1242f22e39be2a9de22329ee6b9de87354203bf8
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-05-19 14:03:32 +0530

    fixed messages failing specs

--- app/routes/review_item/supervisor_messages.js
+++ app/routes/review_item/supervisor_messages.js
@@ -1,12 +1,14 @@
-import MessagesBaseRoute from 'shared-goodcity/routes/messages_base';
+import MessagesBaseRoute from "shared-goodcity/routes/messages_base";
 
 export default MessagesBaseRoute.extend({
   renderTemplate() {
-    this.render('message_template', {controller: 'review_item.supervisor_messages'});
+    this.render("message_template", {
+      controller: "review_item.supervisor_messages"
+    });
   },
 
   afterModel() {
-    var itemId = this.modelFor('review_item').get('id');
-    this.store.query('version', { item_id: itemId, for_item: true });
+    var itemId = this.modelFor("review_item").item.get("id");
+    this.store.query("version", { item_id: itemId, for_item: true });
   }
 });
