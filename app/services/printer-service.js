82e29e28fb7fc83c12b9003dfa569a10311b86f6
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-07-28 18:35:40 +0530

    sorted printers in alphabetical order

--- app/services/printer-service.js
+++ app/services/printer-service.js
@@ -7,7 +7,7 @@ export default Ember.Service.extend({
   allAvailablePrinter() {
     return this.get("store")
       .peekAll("printer")
-      .sortBy("id")
+      .sortBy("name")
       .map(printer => printer.getProperties("name", "id"));
   },
 
