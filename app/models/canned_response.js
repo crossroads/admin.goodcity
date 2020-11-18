10d1d690378098c6c9781a25ad482f9d69751172
bharat619 <bharat.h@kiprosh.com>
2020-11-18 12:59:14 +0530

    :art: Handle UI behaviour when Chinese translation is not available

--- app/models/canned_response.js
+++ app/models/canned_response.js
@@ -3,6 +3,7 @@ import attr from "ember-data/attr";
 
 export default Model.extend({
   name: attr("string"),
+  nameEn: attr("string"),
   content: attr("string"),
   contentEn: attr("string"),
   contentZhTw: attr("string")
