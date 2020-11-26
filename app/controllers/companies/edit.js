6d9bdca10b42d52783aa7cefc4ed1d7adeb0e7ea
shreyas098 <shreyash@kiprosh.com>
2020-11-26 18:44:28 +0530

    QA fixes for adding company to offers

--- app/controllers/companies/edit.js
+++ app/controllers/companies/edit.js
@@ -1,6 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
+  messageBox: Ember.inject.service(),
   actions: {
     back() {
       this.get("model.company").rollbackAttributes();
@@ -10,16 +11,20 @@ export default Ember.Controller.extend({
       );
     },
     updateCompany() {
-      let self = this;
       let company = this.get("model.company");
       let offer = this.get("model");
       company.set("name", this.get("model.company.name"));
       company.set("crmId", this.get("model.company.crmId"));
       company.set("updatedById", this.get("session.currentUser.id"));
 
-      company.save().then(function() {
-        self.transitionToRoute("review_offer.donor_details", offer.get("id"));
-      });
+      company
+        .save()
+        .then(() => {
+          this.transitionToRoute("review_offer.donor_details", offer.get("id"));
+        })
+        .catch(e =>
+          this.get("messageBox").alert(e.errors[0]["detail"].message)
+        );
     }
   }
 });
