b35a6046d9bba272a6782963b2bf4886bb5f768d
abulsayyad123 <abul.a@kiprosh.com>
2021-03-10 13:16:31 +0530

    renamed create-update action name

--- app/components/add-edit-message-overlay.js
+++ app/components/add-edit-message-overlay.js
@@ -25,7 +25,7 @@ export default Ember.Component.extend(AsyncTasksMixin, {
   ),
 
   actions: {
-    createUpdateCannedMessage() {
+    createOrUpdateCannedMessage() {
       this.runTask(
         this.get("message")
           .save()
