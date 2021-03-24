e2fe25305016c60453cf3327995bbc6451fb932f
abulsayyad123 <abul.a@kiprosh.com>
2021-03-24 16:21:50 +0530

    fix: failing test case

--- tests/acceptance/review-item-test.js
+++ tests/acceptance/review-item-test.js
@@ -133,7 +133,7 @@ test("Back button redirects to review offer page", function(assert) {
 
   visit(`/offers/${offerId}/review_item/${itemId}`);
   andThen(function() {
-    click("#backToItem");
+    click(".back");
   });
   andThen(function() {
     assert.equal(currentURL(), `/offers/${offerId}/review_offer/items`);
