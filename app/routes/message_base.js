753708618bf9887b78eebed4555f597ad47c47b2
swatijadhav <swati.nakshtra17@gmail.com>
2021-06-08 11:27:24 +0530

    GCW-3660 Fixed General Messages Thread

--- app/routes/message_base.js
+++ app/routes/message_base.js
@@ -1,6 +1,21 @@
 import AuthorizeRoute from "goodcity/routes/authorize";
+import Ember from "ember";
 
 export default AuthorizeRoute.extend({
+  model() {
+    return Ember.RSVP.Promise.all([
+      this.store.query("message", {
+        messageable_type: "Offer",
+        messageable_id: this.modelFor("offer").get("id")
+      })
+    ]);
+  },
+
+  afterModel() {
+    var offerId = this.modelFor("offer").get("id");
+    this.store.query("version", { item_id: offerId, for_offer: true });
+  },
+
   setupController(controller, model) {
     this._super(controller, model);
     controller.set("isActive", true);
