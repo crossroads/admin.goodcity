f1877d7598e2aa0380b43f45cb19644ffba7c8fa
shreyas098 <shreyash@kiprosh.com>
2021-07-14 12:34:41 +0530

    GCW-3663-Add offer response table changes

--- app/routes/review_offer/share/chat.js
+++ app/routes/review_offer/share/chat.js
@@ -1,10 +1,21 @@
 import MessagesBaseRoute from "../../message_base";
 
 export default MessagesBaseRoute.extend({
+  async model({ user_id }) {
+    this.set("userId", user_id);
+    let offerResponse = await this.store.query("offerResponse", {
+      offer_response: { user_id: user_id, offer_id: this.modelFor("offer").id }
+    });
+    return this.store.query("message", {
+      messageable_type: "OfferResponse",
+      messageable_id: offerResponse.content[0].id
+    });
+  },
+
   setupController(controller, recipient) {
     this._super(controller, recipient);
     controller.set("recipient", recipient);
-    controller.set("recipientId", recipient.get("id"));
+    controller.set("recipientId", this.get("userId"));
     controller.set("offer", this.modelFor("offer"));
   }
 });
--- app/routes/review_offer/share/index.js
+++ app/routes/review_offer/share/index.js
@@ -1,6 +1,23 @@
 import AuthorizeRoute from "../../authorize";
 
 export default AuthorizeRoute.extend({
+  model() {
+    return this.store.query("offerResponse", {
+      offer_response: {
+        offer_id: this.modelFor("offer").id
+      }
+    });
+  },
+
+  afterModel(model) {
+    model.content.map(offerResponse => {
+      this.store.query("message", {
+        messageable_type: "OfferResponse",
+        messageable_id: offerResponse.id
+      });
+    });
+  },
+
   setupController(controller, model) {
     this._super(controller, model);
     controller.set("offer", this.modelFor("offer"));
