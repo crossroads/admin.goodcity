d2237cdce6079f3ce087d542f94c4e221203deab
swatijadhav <swati.nakshtra17@gmail.com>
2021-08-03 10:44:33 +0530

    GCW-3664 refactored datetime component

--- app/components/datetime-selector.js
+++ app/components/datetime-selector.js
@@ -1,20 +1,22 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-  invalidDateTime: Ember.computed("selectedDateTime", function() {
-    return !this.get("selectedDateTime");
-  }),
+  selectedDate: "",
+  selectedTime: "",
 
-  selectedDate: Ember.computed("selectedDateTime", function() {
+  setDate() {
     if (
       this.get("selectedDateTime") &&
       typeof this.get("selectedDateTime") !== "string"
     ) {
-      return moment(this.get("selectedDateTime")).format("DD/MM/YYYY");
+      return this.set(
+        "selectedDate",
+        moment(this.get("selectedDateTime")).format("DD/MM/YYYY")
+      );
     }
-  }),
+  },
 
-  selectedTime: Ember.computed("selectedDateTime", "selectedDate", function() {
+  setTime() {
     if (
       this.get("selectedDateTime") &&
       typeof this.get("selectedDateTime") !== "string"
@@ -23,31 +25,36 @@ export default Ember.Component.extend({
         "en-US",
         { timeZone: "Asia/Hong_Kong" }
       );
-      return moment(date).format("HH:mm");
-    } else if (this.get("selectedDate")) {
-      return "23:59";
+      this.set("selectedTime", moment(date).format("HH:mm"));
     }
+  },
+
+  init() {
+    this._super(...arguments);
+    this.setDate();
+    this.setTime();
+  },
+
+  invalidDateTime: Ember.computed("selectedDateTime", function() {
+    return !this.get("selectedDateTime");
   }),
 
-  stopSharingObserver: Ember.observer(
-    "selectedTime",
-    "selectedDate",
-    function() {
-      let pattern = /(\d{2})\/(\d{2})\/(\d{4})/;
-
+  selectedDateTime: Ember.computed("selectedTime", "selectedDate", {
+    get() {
       if (typeof this.get("selectedDate") === "string") {
+        let pattern = /(\d{2})\/(\d{2})\/(\d{4})/;
         let date = this.get("selectedDate").replace(pattern, "$3/$2/$1");
         let time = this.get("selectedTime") || "23:59";
 
         if (date) {
-          this.set("selectedDateTime", `${date} ${time} HKT`);
-        } else {
-          this.set("selectedTime", undefined);
-          this.set("selectedDateTime", undefined);
+          return `${date} ${time} HKT`;
         }
       }
+    },
+    set(_, value) {
+      return value;
     }
-  ),
+  }),
 
   actions: {
     // Remove when Ember is upgraded to >= 3.0
