482a457e09586ab5c104e284b73daa608c7c1ea0
shreyas098 <shreyash@kiprosh.com>
2021-09-27 10:37:29 +0530

    GCW-3712- Improve shareables unread count

---
+++ app/components/display-offer-list.js
@@ -0,0 +1,39 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  allMessages: Ember.computed(function() {
+    return this.get("store").peekAll("message");
+  }),
+
+  messageThreads: Ember.computed(
+    "offer",
+    "offer.id",
+    "allMessages.[]",
+    "allMessages.length",
+    "allMessages.@each.{senderId,recipientId}",
+    function() {
+      var unreadMessageCount = this.get("offer.unreadMessagesCount")
+        ? this.get("offer.unreadMessagesCount")
+        : 0;
+      let offerResponse = this.get("store")
+        .peekAll("offerResponse")
+        .filterBy("offerId", this.get("offer.id"));
+
+      offerResponse.uniq().map(uid => {
+        let messages = this.get("allMessages").filter(
+          m =>
+            m.get("messageableType") === "OfferResponse" &&
+            m.get("messageableId") === uid.id
+        );
+
+        unreadMessageCount += messages.reduce((sum, m) => {
+          return sum + (m.get("isRead") ? 0 : 1);
+        }, 0);
+      });
+
+      return unreadMessageCount;
+    }
+  )
+});
