89f7b719577411868f29589b1effaacd3f5e68a6
Stephen Kenworthy <steveyken@gmail.com>
2024-01-23 21:17:51 +0800

    Added delete account feature

---
+++ app/routes/my_account.js
@@ -0,0 +1,18 @@
+import AuthorizeRoute from "./authorize";
+
+export default AuthorizeRoute.extend({
+  renderTemplate() {
+    this.render(); // default template
+    this.render("appMenuList", {
+      into: "my_account",
+      outlet: "appMenuList",
+      controller: "offers"
+    });
+  },
+
+  model() {
+    return Ember.RSVP.hash({
+      user: this.store.peekRecord("user", this.session.get("currentUser.id"))
+    });
+  }
+});
